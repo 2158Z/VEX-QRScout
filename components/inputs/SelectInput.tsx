4ab4181f785f358de7f41e851cd93beb766b4ce6
jam41803 <kennysonle5.0@gmail.com>
2024-01-20 05:35:33 -0600

    style

--- components/inputs/SelectInput.tsx
+++ components/inputs/SelectInput.tsx
@@ -13,7 +13,7 @@ export default function SelectInput(data: SelectInputProps) {
   }
   return (
     <select
-      className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none"
+      className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-gray text px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none"
       name={data.title}
       id={data.title}
       onChange={handleSelect}
