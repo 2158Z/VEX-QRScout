58c3580f03464a6ec861571dd0b3c9a6eff3f1c1
jam41803 <kennysonle5.0@gmail.com>
2024-01-20 05:48:48 -0600

    style

--- components/inputs/NumberInput.tsx
+++ components/inputs/NumberInput.tsx
@@ -15,7 +15,7 @@ export default function NumberInput(data: NumberInputProps) {
 
   return (
     <input
-      className="rounded py-2 px-1"
+      className="rounded py-2 px-1 bg-gray-500 text"
       type="number"
       min={data.min}
       max={data.max}
