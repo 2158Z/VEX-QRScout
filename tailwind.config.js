3a74cac787cb762664047644cc698c344b51f65f
jam41803 <kennysonle5.0@gmail.com>
2024-01-20 04:57:18 -0600

    Use old version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-  darkMode: 'class',
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        rhr: ['"SF Sports Night"'],
-        'rhr-ns': ['"SF Sports Night NS"'],
-      },
-      colors: {
-        red: { rhr: '#ef3340' },
-      },
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
 }
